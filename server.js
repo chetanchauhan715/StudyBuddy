acebfb43f4309e01b5925cee6994e8e58d1026f3
Chetan Chauhan <chetan99256@gmail.com>
2026-07-05 11:07:00 +0530

    Add signup API with validation and duplicate email check

--- server.js
+++ server.js
@@ -1,9 +1,41 @@
+const users = [];
+
 import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/"  , (req, res) =>{
     res.send("StudyBuddy API Running ");
+})
+
+app.post("/signup" , (req, res) =>{
+    const {name , email , password} = req.body;
+
+    const newUser = {
+        name,
+        email,
+        password
+    };
+
+    if(!name || !email || !password){
+       return  res.send("please fill all required fields");
+    } 
+
+    for(const user of users){
+        if(user.email.toLowerCase() === newUser.email.toLowerCase()){
+            return res.send(`${newUser.email} alraedy Exist`);
+        }
+    }
+
+    
+    users.push(newUser);
+    console.log(users);
+
+    return res.send("Signup Succesfull");
+
+    
 })
 
 app.listen(3000 , () => {
