e3a02546e744658ab4d52ecb0971d8679c56f677
Chetan Chauhan <chetan99256@gmail.com>
2026-07-09 18:08:35 +0530

    refactor: extract study session logic into controllers

---
+++ controllers/studySessionController.js
@@ -0,0 +1,90 @@
+import e from "express";
+import StudySession from "../models/StudySession.js";
+
+export async function createStudySessions(req,res){
+    const {subject , topic , duration , status} = req.body;
+
+    if(!subject || !duration || !status){
+        return res.status(400).send("Please fill all the requied Fields");
+    }
+
+    const newStudySession = {
+        subject , 
+        topic , 
+        duration,
+        status, 
+        user:req.user.userId
+    }
+
+    try{
+
+        await StudySession.create(newStudySession);
+        console.log("New Study session created Succesfully");
+        return res.status(201).send("Succesfull creation");
+    } catch (error){
+        console.log("CREATE ERROR:");
+        console.log(error);
+        return res.status(500).send(error.message);
+    }
+};
+
+//-------- study session get Functionality 
+
+export async function getStudySessions(req , res){
+    try{
+        const sessions =  await StudySession.find({user:req.user.userId});
+        return res.status(200).send(sessions);
+     }
+     catch (error){
+         console.log(error);
+         res.status(500).send("Unexpected error occurred");
+     }
+};
+
+// --------- study session update functionality 
+
+export async function updateStudySession(req , res){
+    try{
+        const {id} = req.params;
+        const session = await StudySession.findOne({
+            _id:id , 
+            user:req.user.userId
+        });
+        if(!session){
+            
+            return res.status(404).send("Session Not Found");
+        } else {
+            session.status = "Completed";
+            await session.save();
+            return res.status(200).send("Study Session Update Succesfully");
+        }
+        
+
+    } catch (error){
+        console.log(error);
+        return res.status(500).send("Unexpected Error occurred");
+    }
+
+};
+
+
+// ------ delete session functinality 
+
+export async function removeStudySession(req , res){
+    try{
+        const {id} = req.params;
+        const session = await StudySession.findOne({
+            _id:id,
+            user:req.user.userId
+        });
+        if(!session){
+            return res.status(404).send("Study session Not Found");
+        } 
+
+        await session.deleteOne();
+        return res.status(200).send("Study session Deleted succesfully");
+    } catch (error){
+        console.log(error);
+        return res.status(500).send("Unexpected Error Occurred");
+    }
+}
