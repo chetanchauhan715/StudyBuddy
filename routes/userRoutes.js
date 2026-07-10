331af928f48d6182eb3187eb16f530cd63c5a0b7
Chetan Chauhan <chetan99256@gmail.com>
2026-07-10 09:40:02 +0530

    feat: add request validation using express-validator

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,87 +1,25 @@
 import expres from "express";
-import bcrypt from "bcrypt";
-import User from "../models/User.js";
-import jwt from "jsonwebtoken";
+// import bcrypt from "bcrypt";
+// import User from "../models/User.js";
+// import jwt from "jsonwebtoken";
+import { login, signup } from "../controllers/userController.js";
+import { loginValidator, signupValidator } from "../validators/userValidator.js";
 
 
 const router = expres.Router();
 
 
 // ------ sign up API --------
-router.post("/signup" , async (req, res) =>{ 
-
-    if(!name || !email || !password){
-        return res.status(400).send("please fill all required fields");
-     } 
- 
-    const {name , email , password} = req.body;
-
-    const hashedPassword = await bcrypt.hash(password , 10); // password hashing
-
-    const newUser = {
-        name,
-        email,
-        password:hashedPassword
-    };
-
-    
-
-    try{
-
-    const existingUser = await User.findOne( {email});
-    if(existingUser){
-        return res.status(400).send("Email alraedy exist");
-    }
-
-        await User.create(newUser);
-        return res.status(201).send("Signup Succesfull");
-
-    } catch (error) {
-        console.log(error);
-        return res.status(500).send("Something went Wrong");
-    }
-
-});
+router.post("/signup" , signupValidator, signup);
+router.post("/login" , loginValidator,login);
 
 
 
 // --------------- login API --------
-router.post("/login" , async (req, res) =>{
+// router.post("/login" , async (req, res) =>{
 
-    const {email , password} = req.body;
-
-    if(!email || !password){
-        return res.status(400).send("Please Fill all the fileds");
-    }
-
-    try{
-        const existingUser = await User.findOne( {email});
-
-        if(!existingUser){
-            return res.status(404).send("User Not Found");
-        } 
-
-
-        const isMatch = await bcrypt.compare(password , existingUser.password);
-        if(isMatch){
-            // jwt token -------------
-            const token = jwt.sign(
-                {   
-                    userId:existingUser._id
-                },
-                "mySecretKey"
-            );
-
-           return  res.status(200).send(token); // login 
-        }
-
-        return res.status(401).send("Invalid Password");
-
-    } catch (error){
-        console.log(error);
-        res.status(500).send("Unexpected Error Occurred");
-    }
-});
+    
+// });
 
 // - ----------- ---------- export
 export default router;
---
+++ validators/userValidator.js
@@ -0,0 +1,60 @@
+import {body , validationResult} from "express-validator";
+
+export const signupValidator = [
+    body("name")
+    .notEmpty()
+    .withMessage("Name is Required"),
+
+    body("email")
+    .notEmpty()
+    .withMessage("Email is Required")
+    .isEmail()
+    .withMessage("Please Enter Valid email"),
+
+    body("password")
+    .notEmpty()
+    .withMessage("Password is Required")
+    .isLength( {min:8})
+    .withMessage("Password must be atlest 8 characters "),
+
+
+    (req , res , next ) => {
+        const error = validationResult(req);
+
+        if(!error.isEmpty()){
+            return res.status(400).json( {
+            error:error.array()
+    });
+}
+
+        next();
+    }
+];
+
+export const loginValidator = [
+    body("email")
+    .notEmpty()
+    .withMessage("Email is Required")
+    .bail()
+    .isEmail()
+    .withMessage("Enter Valid Email"),
+
+    body("password")
+    .notEmpty()
+    .withMessage("Password is required")
+    .bail()
+    .isLength( {min :8})
+    .withMessage("Password must be atlest 8 characters"),
+
+    (req , res, next) => {
+        const error = validationResult(req);
+
+        if(!error.isEmpty()){
+            return res.status(400).json({
+                error:error.array()
+            });
+        }
+        next();
+    }
+];
+
