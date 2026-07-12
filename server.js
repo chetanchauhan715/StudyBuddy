3d6d6eced0d029cc343f466794118df4ed57d5cf
Chetan Chauhan <chetan99256@gmail.com>
2026-07-12 09:56:22 +0530

    feat: migrate database from local MongoDB to Atlas

--- server.js
+++ server.js
@@ -4,16 +4,18 @@ import dotenv from "dotenv";
 
 import userRoutes from "./routes/userRoutes.js";
 import studySessionRoutes from "./routes/studySessionRoutes.js"; 
-    import errorMiddleware from "./middleware/errorMiddleware.js";
+import errorMiddleware from "./middleware/errorMiddleware.js";
 
 const app = express();
 
 app.use(express.json());
 
+dotenv.config();
+
 // database connection ----
 connectDB();
 
-dotenv.config();
+
 
 app.use(userRoutes); // user routes - sign up & login 
 
@@ -28,7 +30,7 @@ app.get("/"  ,  (req, res) =>{
     res.send("StudyBuddy API Running ");
 });
 
-
-app.listen(3000 , () => {
-    console.log("Server Started");
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
 });
