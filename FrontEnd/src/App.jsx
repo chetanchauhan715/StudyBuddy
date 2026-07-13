e4bade62ef637eb5f1801d178e719fff2a70cfc6
Chetan Chauhan <chetan99256@gmail.com>
2026-07-13 10:21:05 +0530

    feat: set up React Router and initial routing structure

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,7 +1,27 @@
+import { Route, Routes } from "react-router-dom";
+
 function App(){
   return (
     <div>
-      <h1>StudyBuddy</h1>`
+      <h1>StudyBuddy</h1>
+
+      <Routes>
+
+      <Route 
+      path="/"
+      element={<Landing/>}
+      />
+
+      <Route 
+      path="/singup"
+      element={<Signup/>}
+        />
+
+      <Route path="/login"
+      element={<Login/>}
+      />
+
+      </Routes>
     </div>
   );
 }
