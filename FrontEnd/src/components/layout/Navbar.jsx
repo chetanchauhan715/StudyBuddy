2c0984ec0bf8c113646f7d1c65985c21e2340eff
Chetan Chauhan <chetan99256@gmail.com>
2026-07-15 23:11:01 +0530

    feat: add dashboard navbar with search and action icons

---
+++ FrontEnd/src/components/layout/Navbar.jsx
@@ -0,0 +1,27 @@
+import "./Navbar.css";
+import { FaBars , FaBell, FaUserCircle } from "react-icons/fa";
+
+function Navbar(){
+    return(
+        
+        <nav className="navbar-container">
+
+        <div className="navbar-left">
+            <FaBars/>
+        </div>
+
+        <div className="navbar-center">
+        <input type="text" placeholder="Search sessions..."/>
+        </div>
+
+        <div className="navbar-right">
+        <FaBell/>
+        <FaUserCircle/>
+        </div>
+
+        </nav>
+
+    )
+}
+
+export default Navbar;
