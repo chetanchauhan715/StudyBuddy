2b1709a69f412d6d68ef88c6c197608da558cc12
Chetan Chauhan <chetan99256@gmail.com>
2026-07-15 23:13:43 +0530

    refactore: move manual files - sidebar & navbar and use layout

--- FrontEnd/src/pages/Dashboard.jsx
+++ FrontEnd/src/pages/Dashboard.jsx
@@ -1,28 +1,10 @@
-import { useNavigate } from "react-router-dom";
 
 
 
 function Dashboard(){
-
-    const navigate = useNavigate();
-
-    function handleLogout(){
-        
-       localStorage.removeItem("token");
-       navigate("/login");
-
-       
-
-    }
+    
     return(
-
-        <>
         <h1>Dashboard</h1>
-
-        <button onClick={handleLogout}>
-            LogOut
-        </button>
-        </>
     )
 
 }
