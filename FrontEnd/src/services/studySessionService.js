e8e37e1b22e4472fb477faa894eeddbcba72be12
Chetan Chauhan <chetan99256@gmail.com>
2026-07-24 11:51:30 +0530

    refactor: seperae axios instance for profile and session services

--- FrontEnd/src/services/studySessionService.js
+++ FrontEnd/src/services/studySessionService.js
@@ -1,18 +1,4 @@
-import axios from "axios";
-
-const api = axios.create({
-    baseURL:"http://localhost:3000",
-});
-
-api.interceptors.request.use( (config) => {
-    const token = localStorage.getItem("token");
-
-    if(token){
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-
-});
+import api from "../api/axios";
 
 export async function getSessions(filters){
     const response = await api.get("/study-sessions", 
