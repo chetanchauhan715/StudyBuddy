bbc8c66e264ef3bb65aba49d638fdb6dfe7d837b
Chetan Chauhan <chetan99256@gmail.com>
2026-07-27 16:16:13 +0530

    refactor: update logo desing

--- FrontEnd/src/components/logo/Logo.jsx
+++ FrontEnd/src/components/logo/Logo.jsx
@@ -1,8 +1,20 @@
 import "./Logo.css";
-function Logo (){
-    return (
-        <h1>StudyBuddy</h1>
-    )
+
+function Logo() {
+  return (
+    <div className="logo-container">
+
+      <div className="logo-icon">
+        SB
+      </div>
+
+      <div className="logo-text">
+        <span>Study</span>
+        <span>Buddy</span>
+      </div>
+
+    </div>
+  );
 }
 
 export default Logo;
