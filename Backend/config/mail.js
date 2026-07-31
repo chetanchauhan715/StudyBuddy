8456a0595dc4652a68216fec5ee669ca6db5376f
Chetan Chauhan <chetan99256@gmail.com>
2026-07-31 21:12:57 +0530

    fix: smtp gmail server

--- Backend/config/mail.js
+++ Backend/config/mail.js
@@ -1,13 +1,16 @@
-import nodemailder from "nodemailer";
+import nodemailer from "nodemailer";
 
-console.log(process.env.EMAIL_PASS);
-const transporter = nodemailder.createTransport({
-    service:"gmail",
-
-    auth:{
-        user:process.env.EMAIL_USER,
-        pass:process.env.EMAIL_PASS,
-    },
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true only for 465
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default transporter;
